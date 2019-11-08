e53e292af4607e007f47888bd7e682123ce572dc
kxsuhyun <kxsuhyun@gmail.com>
2019-11-08 14:10:27 +0900

    수현 터미널2

--- ddokshik/src/main/java/kr/co/ddokshik/HomeController.java
+++ ddokshik/src/main/java/kr/co/ddokshik/HomeController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	//hi111111
+	//hi11111111
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
