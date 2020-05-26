8c9138d8e591ed25060433f8a26e08a83074cf40
이민우 <mwlee91@douzone.com>
2020-05-26 20:20:23 +0900

    SHA-256 해싱하는 메소드 개발

--- ddokshik/src/main/java/com/spring/board/HomeController.java
+++ ddokshik/src/main/java/com/spring/board/HomeController.java
@@ -4,6 +4,7 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import org.apache.http.util.TextUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -11,29 +12,42 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import kr.co.ddokshik.CommonUtil;
+
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+
 		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+
+		model.addAttribute("serverTime", formattedDate);
+
+		// SHA-256 변환 메소드 사용 예제 시작
+		String passwordToSHA256 = "password";
+		String passwordBySHA256 = CommonUtil.strToSHA256(passwordToSHA256);
+		// SHA-256 변환 실패한 경우 소스 시작
+		if (TextUtils.isEmpty(passwordBySHA256)) {
+			System.out.println("비밀번호 SHA256 해싱 값 : 변환 실패 로직이 있다면 추가해주세요");
+		}
+		// SHA-256 변환 실패한 경우 소스 끝
+		System.out.println("비밀번호 SHA256 해싱 값 : " + passwordBySHA256);
+		// SHA-256 변환 메소드 사용 예제 끝
+
 		return "home";
 	}
-	
+
 }
